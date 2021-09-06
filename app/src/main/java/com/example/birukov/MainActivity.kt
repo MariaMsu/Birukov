92138ad4db7ae36b90401052331b308b89f9f4e0
Alexandra Pervushina <ficstula@gmail.com>
2021-09-06 23:16:49 +0300

    Added food

--- app/src/main/java/com/example/birukov/MainActivity.kt
+++ app/src/main/java/com/example/birukov/MainActivity.kt
@@ -2,10 +2,37 @@ package com.example.birukov
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.widget.EditText
+import android.widget.TextView
+
+class Birukov(food: String) {
+    var food = food
+    fun eatNewFood(): String{
+        return when(food) {
+            "" -> "Golodno :,("
+            "Pizza", "Pasta" -> "Mmmm vkusno :)"
+            else -> "Nu takoe :("
+        }
+    }
+}
+
 
 class MainActivity : AppCompatActivity() {
+    lateinit var Andrew : Birukov
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        Andrew = Birukov("")
+    }
+
+    fun sendFood(view: View) {
+        val editText = findViewById<EditText>(R.id.editTextTextFood)
+        Andrew.food = editText.text.toString()
+
+        val textView = findViewById<TextView>(R.id.textView).apply {
+            text = Andrew.eatNewFood()
+        }
     }
 }
