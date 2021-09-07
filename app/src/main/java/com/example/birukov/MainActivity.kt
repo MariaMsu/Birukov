30c3b4995d178fda4583a1d18c7400584d11a3ae
MariaMsu <i.knaz@yandex.ru>
2021-09-07 11:32:57 +0300

    add 4 face states

--- app/src/main/java/com/example/birukov/MainActivity.kt
+++ app/src/main/java/com/example/birukov/MainActivity.kt
@@ -7,48 +7,70 @@ import android.widget.EditText
 import android.widget.ImageView
 import android.widget.TextView
 
-class Birukov(food: String) {
-    var food = food
-    fun eatNewFood(): String{
-        return when(food) {
-            "" -> "Golodno :,("
-            "Pizza", "Pasta" -> "Mmmm vkusno :)"
-            else -> "Nu takoe :("
+
+enum class Taste {
+    DELICIOUS, DISGUSTING, HUNGRY
+}
+
+class Birukov(private val nFaces: Int) {
+    var faceState : Int = nFaces / 2
+    var textAnswer = ""
+
+    private fun checkTaste(food: String): Taste {
+        return when (food.lowercase()) {
+            "" -> Taste.HUNGRY
+            "pizza", "pasta" -> Taste.DELICIOUS
+            else -> Taste.DISGUSTING
+        }
+    }
+
+    fun updateEatenFood(food: String) {
+        val taste = checkTaste(food)
+
+        textAnswer = when (taste) {
+            Taste.HUNGRY -> "Golodno :,("
+            Taste.DELICIOUS -> "Mmmm vkusno :)"
+            Taste.DISGUSTING -> "Nu takoe :("
+        }
+
+        faceState = when (taste){
+            Taste.HUNGRY -> maxOf(0, faceState-1)
+            Taste.DELICIOUS -> minOf(nFaces-1, faceState+1)
+            Taste.DISGUSTING -> maxOf(0, faceState-1)
         }
     }
 }
 
 class MainActivity : AppCompatActivity() {
-    lateinit var Andrew : Birukov
-    var faceState = 1
+    private lateinit var Andrew: Birukov
+    private val faceImgs = arrayOf(
+        R.drawable.bir_face_0,
+        R.drawable.bir_face_1,
+        R.drawable.bir_face_2,
+        R.drawable.bir_face_3,
+    )
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        Andrew = Birukov("")
+        Andrew = Birukov(nFaces = faceImgs.size)
     }
 
-    private fun updateTextAnswer(){
-        val editText = findViewById<EditText>(R.id.editTextTextFood)
-        Andrew.food = editText.text.toString()
-
-        val textView = findViewById<TextView>(R.id.textView).apply {
-            text = Andrew.eatNewFood()
+    private fun updateTextAnswer() {
+        findViewById<TextView>(R.id.textView).apply {
+            text = Andrew.textAnswer
         }
     }
 
-    private fun updatePicture(){
-        faceState += 1  // todo change logic
+    private fun updatePicture() {
         val editImg = findViewById<ImageView>(R.id.imageViewFace)
-
-        if (faceState % 2 == 0){
-            editImg.setImageResource(R.drawable.bir_test_0)
-        } else {
-            editImg.setImageResource(R.drawable.bir_test_1)
-        }
+        editImg.setImageResource(faceImgs[Andrew.faceState])
     }
 
     fun sendFood(view: View) {
+        val editText = findViewById<EditText>(R.id.editTextTextFood)
+        Andrew.updateEatenFood(editText.text.toString())
+
         updateTextAnswer()
         updatePicture()
     }
